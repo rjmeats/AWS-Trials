a603aabb079759b94662d5fccf97897dab1c0d61
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-02-06 16:39:27 +0000

    CLI S3 examples

---
+++ AWSTrials/CLI/s3/buckets.sh
@@ -0,0 +1,61 @@
+. ../aws_env_setup.sh
+
+echo
+echo $SHELL at $(date)
+
+echo
+echo "List buckets:"
+echo
+
+aws s3api list-buckets
+
+MY_BUCKET="rjm-cli-test-bucket"
+
+echo
+echo "Delete ${MY_BUCKET} bucket, may not exist:"
+echo
+
+aws s3api delete-bucket --bucket "${MY_BUCKET}"
+
+echo
+echo "Create new ${MY_BUCKET} bucket:"
+echo
+echo "- in London region"
+echo
+
+aws s3api create-bucket --bucket "${MY_BUCKET}" --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+
+echo
+echo "List buckets:"
+echo
+
+aws s3api list-buckets
+
+echo
+echo "Try to create ${MY_BUCKET} bucket again:"
+echo
+echo "- expect an 'already exists and you own it' error"
+echo
+
+aws s3api create-bucket --bucket "${MY_BUCKET}" --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+
+
+echo
+echo "Try to create 'rjm' bucket again:"
+echo
+echo "- expect an 'can't create, already exists' error, not owned by me"
+echo
+
+aws s3api create-bucket --bucket rjm --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+
+echo
+echo "Delete ${MY_BUCKET} bucket to tidy up:"
+echo
+
+aws s3api delete-bucket --bucket "${MY_BUCKET}"
+
+echo
+echo "List buckets:"
+echo
+
+aws s3api list-buckets
