feeb5e4462b5de374942118cd3ee0255fc1c0a2e
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-02-07 17:37:03 +0000

    CLI EC2 examples

--- AWSTrials/CLI/ec2/ec2.sh
+++ AWSTrials/CLI/ec2/ec2.sh
@@ -8,3 +8,11 @@ echo "Describe instances:"
 echo
 
 aws ec2 describe-instances
+
+echo
+echo "Describe AMI images from amazon:"
+echo
+echo "- just show the first 50 lines of vast amounts of output"
+echo
+
+aws ec2 describe-images --owners amazon | head -50
