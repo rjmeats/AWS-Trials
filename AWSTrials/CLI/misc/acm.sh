a5991f1a563a7aa9af9b7c34648028f8d087b929
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-03-04 12:48:02 +0000

    Domain web site files

---
+++ AWSTrials/CLI/misc/acm.sh
@@ -0,0 +1,24 @@
+. ../aws_env_setup.sh
+
+echo
+echo $SHELL at $(date)
+
+echo
+echo "List certificates:"
+echo
+echo "- US East 1 (North Virginia) region"
+echo 
+
+aws acm list-certificates --region us-east-1
+
+CERT_ARN=$(aws acm list-certificates --region us-east-1 --output text | cut -f2)
+
+echo
+echo "Describe certificate:"
+echo
+echo "- US East 1 (North Virginia) region"
+echo "- ARN: $CERT_ARN"
+echo 
+
+aws acm describe-certificate --region us-east-1 --certificate-arn $CERT_ARN
+
