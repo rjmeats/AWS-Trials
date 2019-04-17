1614877f8552456622f60054aeec5c37770dbb30
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-04-17 16:18:26 +0100

    Python trials

--- AWSTrials/Python/session_info.py
+++ AWSTrials/Python/session_info.py
@@ -4,6 +4,37 @@ import boto3
 
 s = boto3.Session()
 
+print()
 print("Available services:")
 print()
 print(s.get_available_services())
+
+print()
+print("Available resources:")
+print()
+print(s.get_available_resources())
+
+print()
+print("Available regions for S3:")
+print()
+print(s.get_available_regions('s3'))
+
+print()
+print("Available partitions:")
+print()
+print(s.get_available_partitions())
+
+print()
+print("Credentials:")
+print()
+print(s.get_credentials())
+
+print()
+print("Available profiles:")
+print()
+print(s.available_profiles)
+
+print()
+print("Profile and region:")
+print()
+print(s.profile_name, s.region_name)
