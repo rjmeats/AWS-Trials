9dd9707a906b7d60c95466da6805a72478e58b55
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-05-02 09:57:52 +0100

    Python lambda trials

--- AWSTrials/Python/lambdatest3/test3.py
+++ AWSTrials/Python/lambdatest3/test3.py
@@ -4,27 +4,43 @@ import requests
 def lambda_handler(event, context):
 
 	urlString = event['site']
-	r = requests.get(urlString)
-	bytes = r.text
+	try :
+		r = requests.get(urlString, timeout=10)
+		if r.status_code != 200:
+			return {
+				'url' : urlString,
+				'status' : r.status_code
+			}
+		else :
+			bytes = r.text
+	except requests.ConnectionError as e:
+		return {
+			'url' : urlString,
+			'error' : str(e)		
+		}
 
-	#bytes = "<html><head><title>My title</title></head><body></body></html>"
+	# bytes = "<html><head><title>My title</title></head><body></body></html>"
 
 	soup = BeautifulSoup(bytes, features="html.parser")
 	results = soup.find_all("title")
-	title = results[0].get_text()
+	if len(results) == 0:
+		title = '[No title found]'
+	else :
+		title = results[0].get_text()
 
 	#print(results)
 	#print(title)
 
 	return {
 		'url' : urlString,
-		'title' : title
+		'title' : title,
+		'status' : r.status_code
 	}
 
 
 if __name__ == "__main__" :
 	#print("Running as main")
-	param = { "site" : "https://www.reuters.com" }
+	param = { "site" : "https://www.bbc.co.uk/" }
 	ret = lambda_handler(param, 0)
 	print(ret)
 
