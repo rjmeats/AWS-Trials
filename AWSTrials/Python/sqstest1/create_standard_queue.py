c689d138d6382c20873860dad7acba2871ebf3f7
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-05-02 17:04:01 +0100

    Python SQS trials

---
+++ AWSTrials/Python/sqstest1/create_standard_queue.py
@@ -0,0 +1,45 @@
+import time
+import boto3
+import list_queues as lq
+
+sqs = boto3.resource('sqs')
+
+def queue_exists(qname) :
+	qlist = sqs.queues.filter(QueueNamePrefix=qname)
+	for q in qlist :
+		if lq.extractQueueNameFromArn(q.attributes['QueueArn']) == qname :
+			return True
+	return False
+
+def create_queue(qname) :
+	print("Creating new queue called:", qname)
+
+	# Does our queue already exist ? NB this throws an exception if a queue of this name does not exist.
+	if queue_exists(qname) :
+		print(".. queue already exists")
+		q = sqs.get_queue_by_name(QueueName=qname)
+	else :
+		attrs = {}
+		q = sqs.create_queue(QueueName=qname, Attributes=attrs)
+		print(".. new queue being created ...", end="", flush=True)
+	
+		# Wait for the new queue to become listed
+		waiting = True
+		while waiting :
+			time.sleep(2)
+			if queue_exists(qname) :
+				waiting = False
+				print()
+			else :
+				print('.', end="", flush=True)
+
+	print("Queue available:", q)
+
+if __name__ == "__main__" :
+	print(sqs)
+	print()
+	lq.list_queues()
+	print()
+	create_queue('test_std_q6')
+	print()
+	lq.list_queues()
---
+++ AWSTrials/Python/sqstest1/list_queues.py
@@ -0,0 +1,26 @@
+import time
+import boto3
+
+sqs = boto3.resource('sqs')
+
+def extractQueueNameFromArn(Arn) :
+	return Arn.split(':')[-1]
+
+def list_queues() :
+	qlist = sqs.queues.all()
+	for q in qlist :
+		a = q.attributes
+		print("Queue:", q.url)
+		print("- ARN:", a['QueueArn'])
+		# Looks like queue name has to be extracted from the end of the URL or Arn, no separate attribute/get call
+		qname = extractQueueNameFromArn(a['QueueArn'])
+		print("- name:", qname)
+		print("- msgs:", a['ApproximateNumberOfMessages'])
+		print("- not vis msgs:", a['ApproximateNumberOfMessagesNotVisible'])
+		#print("- ", a)
+
+if __name__ == "__main__" :
+	print(sqs)
+	print()
+	list_queues()
+	print()
