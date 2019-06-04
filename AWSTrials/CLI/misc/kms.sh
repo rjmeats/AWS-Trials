2dffd3c7e440f2441a333a0b3a0f7958e30bb10b
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-06-04 12:52:50 +0100

    CLI KMS

--- AWSTrials/CLI/misc/kms.sh
+++ AWSTrials/CLI/misc/kms.sh
@@ -9,3 +9,20 @@ echo
 
 aws kms list-keys
 
+echo
+echo "List aliases:"
+echo
+
+aws kms list-aliases
+
+echo
+echo "Describe keys:"
+echo
+
+for k in `aws kms list-keys --output text | cut -f2`
+do
+	echo Key $k
+	echo
+	aws kms describe-key --key-id "$k"
+done
+
