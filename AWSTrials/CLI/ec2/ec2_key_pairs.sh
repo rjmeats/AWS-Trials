5181c8603dc44d507bd0407b6f050d38d6bc0352
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-06-11 11:37:11 +0100

    CLI EC2 Key Pairs

--- AWSTrials/CLI/ec2/ec2_key_pairs.sh
+++ AWSTrials/CLI/ec2/ec2_key_pairs.sh
@@ -8,3 +8,28 @@ echo "Describe EC2 Key Pairs:"
 echo
 
 aws ec2 describe-key-pairs
+
+echo
+echo "Create an EC2 Key Pair:"
+echo
+
+aws ec2 create-key-pair --key-name "kpfromcli"
+
+echo
+echo "Describe EC2 Key Pairs:"
+echo
+
+aws ec2 describe-key-pairs
+
+echo
+echo "Delete an EC2 Key Pair:"
+echo
+
+aws ec2 delete-key-pair --key-name "kpfromcli"
+
+echo
+echo "Describe EC2 Key Pairs:"
+echo
+
+aws ec2 describe-key-pairs
+
