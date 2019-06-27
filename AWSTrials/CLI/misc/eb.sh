227b4b7416f536fcdb40421bb52d7dbb96f70f25
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-06-27 10:17:41 +0100

    ElasticBeanstalk examples

---
+++ AWSTrials/CLI/misc/eb.sh
@@ -0,0 +1,29 @@
+. ../aws_env_setup.sh
+
+echo
+echo $SHELL at $(date)
+
+echo
+echo "Check DNS availability:"
+echo
+
+aws elasticbeanstalk check-dns-availability --cname-prefix abcd 
+
+echo
+echo "Check DNS availability:"
+echo
+
+aws elasticbeanstalk check-dns-availability --cname-prefix rjrj 
+
+echo
+echo "Describe applications:"
+echo
+
+aws elasticbeanstalk describe-applications
+
+echo
+echo "Describe environments:"
+echo
+
+aws elasticbeanstalk describe-environments
+
