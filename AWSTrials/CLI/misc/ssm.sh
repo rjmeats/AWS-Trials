479da061afdc074141309b4d769bf8cc92984a5a
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-09-16 18:01:56 +0100

    ssm

--- AWSTrials/CLI/misc/ssm.sh
+++ AWSTrials/CLI/misc/ssm.sh
@@ -9,3 +9,15 @@ echo
 
 aws ssm describe-parameters
 
+
+echo
+echo "List Amazon Linux Parameter Store namespaces:"
+echo
+echo "for the default region"
+echo
+
+# NB Doesn't run properly when run direct from Bash - not clear why, puts a C:\..... prefix onto path !
+
+powershell 'aws ssm get-parameters-by-path --path "/aws/service/ami-amazon-linux-latest"'
+
+
