e71c448dd6ad5cff805a169942d4f2c6d6e7f46d
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2019-11-06 09:51:36 +0000

    Start notebook

---
+++ AWSTrials/CLI/sagemaker/start_notebook.sh
@@ -0,0 +1,72 @@
+BAD_PARAMETERS=N
+
+if [[ ! -z "$1" ]]
+then
+	REGION="$1"
+	echo
+	echo "Setting default region to $REGION"
+	export AWS_DEFAULT_REGION="${REGION}"
+
+	if [[ ! -z "$2" ]]
+	then
+		NOTEBOOK="$2"
+		echo
+		echo "Using notebook $NOTEBOOK"
+	else
+		BAD_PARAMETERS=Y
+		echo
+		echo "No notebook specified"	
+	fi
+else
+	BAD_PARAMETERS=Y
+	echo
+	echo "No region specified"
+fi
+
+if [[ $BAD_PARAMETERS == "Y" ]]
+then
+	echo
+	echo "Usage is $0 <region> <notebookname>"
+	echo
+	exit 1
+fi
+
+. ../aws_env_setup.sh
+
+echo
+echo $SHELL at $(date)
+
+echo
+echo "Notebook instance details:"
+echo
+
+aws sagemaker describe-notebook-instance --notebook-instance-name $NOTEBOOK
+
+if [[ $? -ne 0 ]]
+then
+	echo
+	echo "Error describing notebook $NOTEBOOK"
+	echo
+	exit 1
+fi
+
+echo
+echo "Starting notebook :"
+echo
+
+aws sagemaker start-notebook-instance --notebook-instance-name $NOTEBOOK
+
+if [[ $? -ne 0 ]]
+then
+	echo
+	echo "Error starting notebook $NOTEBOOK"
+	echo
+	exit 1
+fi
+
+sleep 5
+
+echo "Checking status ..."
+echo
+aws sagemaker describe-notebook-instance --notebook-instance-name $NOTEBOOK
+
